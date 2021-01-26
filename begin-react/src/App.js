c62073c6b2cef00207243f0e0dc24186e51609a7
zeromountain <sonyeongsan@gmail.com>
2021-01-27 01:15:25 +0900

    JSX

---
+++ begin-react/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Hello from './Hello';
+import './App.css';
+
+function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
+  return (
+    <>
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className='gray-box'></div>
+    </>
+  );
+}
+
+export default App;
---
+++ begin-react/src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// function Component
+// Component's first letter is upper case.
+function Hello() {
+  return <div>안녕하세요</div>
+} 
+
+export default Hello;
