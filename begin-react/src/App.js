bbee82a87ccaf4cb834499631bf7000b8bea987c
zeromountain <sonyeongsan@gmail.com>
2021-01-27 21:02:01 +0900

    add item at array

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,10 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import CreateUser from './components/CreatUser';
 import UserList from './components/UserList';
 
 
 function App() {
 
-  const users = [
+  const [ inputs, setInputs ] = useState({
+    username:'',
+    email:'',
+  });
+
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [ name ]: value
+    })
+  }
+
+  const [ users, setUsers ] = useState([
     {
       id: 1,
       username: 'zeromountain',
@@ -20,18 +35,39 @@ function App() {
       username: 'liz',
       email: 'public.liz@gamil.com'
     }
-  ]
- 
+  ]);
+
   const nextId = useRef(4);
 
   const onCreate = () => {
-
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // existing element of array maintain and add new element
+    // setUsers([...users, user]);
+    setUsers(users.concat(user));
+    setInputs({
+      username: '',
+      email: ''
+    })
     console.log(nextId.current);
     nextId.current += 1;
   }
 
+
+
   return (
-    <UserList users={users} />
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
   );
 }
 
