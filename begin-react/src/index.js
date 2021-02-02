c88127f17c9645c5b749611087f50d1249c9ad61
zeromountain <sonyeongsan@gmail.com>
2021-02-03 00:30:12 +0900

    Context API

--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ContextSample from './components/contextSample';
 // import Counter from './components/Counter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextSample />
+    {/* <App /> */}
     {/* <Counter /> */}
   </React.StrictMode>,
   document.getElementById('root')
